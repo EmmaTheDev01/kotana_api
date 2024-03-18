5c847bfc59b6b3e02f43120c86d9c259a747efe1
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-03-18 10:11:57 +0300

    Initial commit

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,5 +1,4 @@
 import jwt from "jsonwebtoken";
-
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.accessToken;
     if (!token) {
@@ -11,16 +10,22 @@ export const verifyToken = (req, res, next) => {
     // Verify the token
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
         if (err) {
-            return res.status(401).json({
-                success: false,
-                message: 'Invalid token',
-            });
+            if (err.name === 'TokenExpiredError') {
+                return res.status(401).json({
+                    success: false,
+                    message: 'Token has expired',
+                });
+            } else {
+                return res.status(401).json({
+                    success: false,
+                    message: 'Invalid token',
+                });
+            }
         }
         req.user = user;
         next();
     });
 };
-
 export const verifyUser = (req, res, next) => {
     if (req.user.id === req.params.id || req.user.role === 'admin') {
         next();
