b48f710ec5fdb183dbd1f236c84360eb76fcce3e
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-04-01 16:03:41 +0300

    Initial commit

--- models/User.js
+++ models/User.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
         enum: ['user', 'admin'],
         default: 'user'
     },
+    score: {
+        type: Number,
+        default: 0
+    },
     online: {
         type: Boolean,
         default: false
