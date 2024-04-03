973b366678aaa885df68ec29f83be1c5d478fbf8
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-04-03 16:09:09 +0300

    Fixing the game controller

--- models/Game.js
+++ models/Game.js
@@ -5,11 +5,11 @@ const gameSchema = new mongoose.Schema({
   players: [
     {
       userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-      position: { type: String, enum: ['player 1', 'player 2'] },
+      position: { type: String, enum: ['player 1', 'player 2'], required: true },
     },
     {
       userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-      position: { type: String, enum: ['player 1', 'player 2'] },
+      position: { type: String, enum: ['player 1', 'player 2'], required: true },
     }
   ],
   scores: [
